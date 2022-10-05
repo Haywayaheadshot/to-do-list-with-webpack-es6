36803c99df87e9d250586f29e06a2f53796ca669
Abubakar Ummar <Haywaya@Abubakars-MacBook-Pro.local>
2022-10-05 20:39:59 +0000

    Create module for Functions

--- src/js/modules/Function.js
+++ src/js/modules/Function.js
@@ -1,16 +1,51 @@
-import {
-  toDoList, parser,
-} from './Variables.js';
+// import {
+//   toDoList, parser, input,
+// } from './Variables.js';
 
-const addTask = (task) => {
-  const li = parser.parseFromString(`
-      <li class="to-do-list-item">
-          <input type="checkbox" class="checkbox" id="checkbox-${task.index}" ${task.completed ? 'checked' : ''}>
-          <label for="checkbox-${task.index}" class="checkbox-label">${task.description}</label>
-          <i class="fas fa-ellipsis-v"></i>
-      </li>
-      `, 'text/html').body.firstChild;
-  toDoList.appendChild(li);
-};
+// // import delete from images
+// import deleteImage from './images/icons/delete.png';
 
-export default addTask;
+// const addTask = (task) => {
+//   const li = parser.parseFromString(`
+//       <li class="to-do-list-item">
+//           <input type="checkbox" class="checkbox" id="checkbox-${task.index}"
+// ${task.completed ? 'checked' : ''}>
+//           <label for="checkbox-${task.index}" class="checkbox-label">${task.description}</label>
+//           <i class="fas fa-ellipsis-v"></i>
+//       </li>
+//       `, 'text/html').body.firstChild;
+//   toDoList.appendChild(li);
+// };
+
+// const createTaskDynamically = () => {
+//   const task = newTask.addNewTask(null, input.value, false);
+//   const li = document.createElement('li');
+//   li.classList.add('to-do-list-item');
+//   toDoList.appendChild(li);
+//   const checkBox = document.createElement('input');
+//   checkBox.setAttribute('type', 'checkbox');
+//   checkBox.setAttribute('class', 'checkbox');
+//   checkBox.setAttribute('id', `checkbox-${task.index}`);
+//   checkBox.setAttribute('unchecked', `${task.completed ? 'checked' : ''}`);
+//   li.appendChild(checkBox);
+//   const checkBoxLabel = document.createElement('label');
+//   checkBoxLabel.setAttribute('for', `checkbox-${task.index}`);
+//   checkBoxLabel.setAttribute('class', 'checkbox-label');
+//   checkBoxLabel.innerHTML = task.description;
+//   li.appendChild(checkBoxLabel);
+//   const editButton = document.createElement('button');
+//   editButton.setAttribute('class', 'edit-button');
+//   editButton.setAttribute('type', 'button');
+//   editButton.innerHTML = `<img class="edit-image" src='${editImage}' alt="Edit Button">`;
+//   li.appendChild(editButton);
+//   const deleteButton = document.createElement('button');
+//   deleteButton.setAttribute('class', 'delete-button');
+//   deleteButton.setAttribute('type', 'button');
+//   deleteButton.innerHTML = `<img class="delete-image" src='${deleteImage}' alt="Delete Button">`;
+//   li.appendChild(deleteButton);
+//   input.value = '';
+//   // get the list from local storage
+//   localStorage.getItem('tasks');
+// };
+
+// export { addTask, createTaskDynamically };
