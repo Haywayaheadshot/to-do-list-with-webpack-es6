515226314ff17299c360828e2181aac1d4bed678
Abubakar Ummar <Haywaya@Abubakars-MacBook-Pro.local>
2022-10-06 08:11:56 +0000

    Add Event listener to delete task

--- src/index.js
+++ src/index.js
@@ -3,17 +3,16 @@ import './style.css';
 import refreshImage from './images/icons/refresh.png';
 // import enter from images
 import enterImage from './images/icons/enter.png';
-// import delete from images
-import deleteImage from './images/icons/delete.png';
-// import edit from images
-import editImage from './images/icons/edit.png';
 
 // import variables from variables.js
 import {
   refresh, submit,
   enterButton, refreshButton, toDoList,
   clearCompletedButton, input, errorMessage,
-} from './js/modules/Variables.js';
+} from './modules/Variables.js';
+
+// import createTaskDynamically from Function.js
+import createTaskDynamically from './modules/Function.js';
 
 refreshButton.src = refreshImage;
 refreshButton.classList.add('refresh-img');
@@ -23,66 +22,6 @@ enterButton.src = enterImage;
 enterButton.classList.add('enter-img');
 // Append enter button to the DOM
 submit.appendChild(enterButton);
-
-// Class for task
-class Task {
-  constructor(index, description, completed = false) {
-    this.index = index;
-    this.description = description;
-    this.completed = completed;
-    toDoList.style.display = 'block';
-    // get the list from local storage
-    this.listArray = JSON.parse(localStorage.getItem('books')) || [];
-  }
-
-  addNewTask = (index = null, description = null, completed = null) => {
-    if (description) {
-      const newTaskIndex = index || this.listArray.length + 1;
-      const isNewTaskCompleted = completed || false;
-      const newTask = new Task(newTaskIndex, description, isNewTaskCompleted);
-      this.listArray.push(newTask);
-      localStorage.setItem('tasks', JSON.stringify(this.listArray));
-      return newTask;
-    }
-
-    return null;
-  };
-}
-
-const newTask = new Task();
-
-const createTaskDynamically = () => {
-  const task = newTask.addNewTask(null, input.value, false);
-  const li = document.createElement('li');
-  li.classList.add('to-do-list-item');
-  toDoList.appendChild(li);
-  const checkBox = document.createElement('input');
-  checkBox.setAttribute('type', 'checkbox');
-  checkBox.setAttribute('class', 'checkbox');
-  checkBox.setAttribute('id', `checkbox-${task.index}`);
-  checkBox.setAttribute('unchecked', `${task.completed ? 'checked' : ''}`);
-  li.appendChild(checkBox);
-  const checkBoxLabel = document.createElement('label');
-  checkBoxLabel.setAttribute('for', `checkbox-${task.index}`);
-  checkBoxLabel.setAttribute('class', 'checkbox-label');
-  checkBoxLabel.innerHTML = task.description;
-  li.appendChild(checkBoxLabel);
-  const editButton = document.createElement('button');
-  editButton.setAttribute('class', 'edit-button');
-  editButton.setAttribute('type', 'button');
-  editButton.innerHTML = `<img class="edit-image" src='${editImage}' alt="Edit Button">`;
-  li.appendChild(editButton);
-  const deleteButton = document.createElement('button');
-  deleteButton.setAttribute('class', 'delete-button');
-  deleteButton.setAttribute('type', 'button');
-  deleteButton.innerHTML = `<img class="delete-image" src='${deleteImage}' alt="Delete Button">`;
-  li.appendChild(deleteButton);
-  input.value = '';
-  // get the list from local storage
-  localStorage.getItem('tasks');
-};
-
-document.addEventListener('DOMContentLoaded', createTaskDynamically.displayTasks);
 
 // event listener for enter button
 submit.addEventListener('click', (event) => {
@@ -102,8 +41,6 @@ refresh.addEventListener('click', (event) => {
   event.preventDefault();
   window.location.reload();
   toDoList.style.display = 'block';
-  // get the list from local storage
-  localStorage.getItem('tasks');
 });
 
 // event listener for clear completed button
@@ -120,6 +57,12 @@ toDoList.addEventListener('click', (event) => {
   if (event.target.className === 'delete-image') {
     // remove task from the DOM
     event.target.parentElement.parentElement.remove();
+    // remove task from local storage
+    const taskIndex = event.target.parentElement.parentElement.children[0].id;
+    const taskIndexNumber = taskIndex.split('-')[1];
+    const tasks = JSON.parse(localStorage.getItem('tasks'));
+    tasks.splice(taskIndexNumber - 1, 1);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   }
   if (event.target.className === 'edit-image') {
     const editButton = event.target.parentElement;
